985c6ab23cc8d82ba0a46c33225e86b94c912125
Bender0239 <hayden.bender@protonmail.com>
2020-08-12 15:51:46 -0700

    almost setup

---
+++ app/src/reducers/daylightReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    sunrise: "",
+    sunset: "",
+    dayLength: "",
+    solarNoon: ""
+}
+
+export const daylightReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            return state
+    }
+}
